fda93aa1a543debfc6c8eefc18c857b2132aea6d
kzshantonu <kzshantonu@users.noreply.github.com>
2022-01-31 04:56:51 +0100

    Version control is awful

--- fresh.sh
+++ fresh.sh
@@ -62,7 +62,6 @@ pip3 install borgmatic
 
 ## install acme.sh with certificates dir set to /certs
 
-touch ~/.zshrc
 mkdir /certs
 git clone https://github.com/acmesh-official/acme.sh.git
 cd acme.sh
@@ -91,6 +90,10 @@ wget -qO - https://raw.githubusercontent.com/CupCakeArmy/autorestic/master/insta
 ## install rclone
 
 curl https://rclone.org/install.sh | bash
+
+## ssh hardening based on https://www.sshaudit.com/hardening_guides.html#ubuntu_20_04_lts
+
+rm /etc/ssh/ssh_host_* && ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N "" && ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N "" && awk '$5 >= 3071' /etc/ssh/moduli > /etc/ssh/moduli.safe && mv /etc/ssh/moduli.safe /etc/ssh/moduli && sed -i 's/^\#HostKey \/etc\/ssh\/ssh_host_\(rsa\|ed25519\)_key$/HostKey \/etc\/ssh\/ssh_host_\1_key/g' /etc/ssh/sshd_config && echo -e "\n# Restrict key exchange, cipher, and MAC algorithms, as per sshaudit.com\n# hardening guide.\nKexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group-exchange-sha256\nCiphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr\nMACs hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-128-etm@openssh.com\nHostKeyAlgorithms ssh-ed25519,ssh-ed25519-cert-v01@openssh.com,sk-ssh-ed25519@openssh.com,sk-ssh-ed25519-cert-v01@openssh.com,rsa-sha2-256,rsa-sha2-512,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-512-cert-v01@openssh.com" > /etc/ssh/sshd_config.d/ssh-audit_hardening.conf && service ssh restart
 
 ## install ohmyzsh
 
